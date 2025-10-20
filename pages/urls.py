2ee9692ed58d06fb8774dda3c1bc112c7db89b77
peterkahumu <muhumukip@gmail.com>
2025-10-20 05:42:02 +0300

    style: unify string quotes to double quotes and improve code formatting in views, urls, and tests

--- pages/urls.py
+++ pages/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from .views import HomeView, AboutPageView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('about/', AboutPageView.as_view(), name='about'),
+    path("", HomeView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
 ]
-
-
--- pages/views.py
+++ pages/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 # Create your views here.
 class HomeView(TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
 
     def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context['inventory_list'] = ['FlatBread', 'Sweet Potatoes', 'Deep fried dough (Mandazi)', 'Eggs']
-        context['title'] = 'Home'
+        context = super().get_context_data(**kwargs)
+        context["inventory_list"] = [
+            "FlatBread",
+            "Sweet Potatoes",
+            "Deep fried dough (Mandazi)",
+            "Eggs",
+        ]
+        context["title"] = "Home"
         return context
 
+
 class AboutPageView(TemplateView):
-    template_name = 'about.html'
+    template_name = "about.html"
 
     def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context['address'] = "123 Johnson Street"
-        context['phoneNumber'] = '+25412345678'
-        context['title'] = "About"
+        context = super().get_context_data(**kwargs)
+        context["address"] = "123 Johnson Street"
+        context["phoneNumber"] = "+25412345678"
+        context["title"] = "About"
         return context
